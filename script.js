cf03c34f9bc17765b4ca76fa1faa5ba597add639
Tulkin <tulkin.boynazarov@gmail.com>
2022-03-22 15:24:27 +0500

    цикл в цикле

---
+++ script.js
@@ -0,0 +1,69 @@
+// for(i = 1; i <=9; i++){  
+//     for(k = 1;k <= 9; k++){
+//         document.write(`${i} * ${k} = ${i * k} <br>` );
+        
+//     }
+//     document.write(` <br>` );
+// }
+
+
+for(let i  = 1; i <= 1; i++){
+    for(let k = 1; k <= 5; k++) {
+        document.write(i) 
+    }
+    document.write(`<br>`) 
+}
+for(let i  = 1; i <= 1; i++){
+    for(let k = 1; k <= 4; k++) {
+        document.write(i) 
+    }
+    document.write(`<br>`) 
+}
+for(let i  = 1; i <= 1; i++){
+    for(let k = 1; k <= 3; k++) {
+        document.write(i) 
+    }
+    document.write(`<br>`)
+}
+for(let i  = 1; i <= 1; i++){
+    for(let k = 1; k <= 2; k++) {
+        document.write(i) 
+    }
+    document.write(`<br>`)
+}
+for(let i  = 1; i <= 1; i++){
+    for(let k = 1; k <= 1; k++) {
+        document.write(i)  
+    }
+    document.write(`<br>`)
+} 
+for(let i  = 1; i <= 1; i++){
+    for(let k = 1; k <= 1; k++) {
+        document.write(i) 
+    }
+    document.write(`<br>`) 
+}
+for(let i  = 1; i <= 1; i++){
+    for(let k = 1; k <= 2; k++) {
+        document.write(i) 
+    }
+    document.write(`<br>`) 
+}
+for(let i  = 1; i <= 1; i++){
+    for(let k = 1; k <= 3; k++) {
+        document.write(i) 
+    }
+    document.write(`<br>`)
+}
+for(let i  = 1; i <= 1; i++){
+    for(let k = 1; k <= 4; k++) {
+        document.write(i) 
+    }
+    document.write(`<br>`)
+}
+for(let i  = 1; i <= 1; i++){
+    for(let k = 1; k <= 5; k++) {
+        document.write(i)  
+    }
+    document.write(`<br>`)
+} 
